99917f8dcb3b28dfd0499a14b94ae39fe1841ea3
Ilia Kaziamov <teacher.ilya.255@gmail.com>
2022-09-09 02:12:12 +0300

    Add new file

---
+++ homework_50/example.py
@@ -0,0 +1,22 @@
+# Import third-party modules
+import pandas
+
+
+PATH_TO_FILE = 'squirrel_data.csv'
+
+
+def save_example_data(path_to_file):
+    """Считает количество белок по цветам и сохраняет данные в файл"""
+    data_squrrels = pandas.read_csv(path_to_file)
+
+    fur_color_black = len(data_squrrels[data_squrrels['Primary Fur Color'] == 'Black'])
+    fur_color_gray = len(data_squrrels[data_squrrels['Primary Fur Color'] == 'Gray'])
+    fur_color_cinnamon = len(data_squrrels[data_squrrels['Primary Fur Color'] == 'Cinnamon'])
+
+    data_dict = {
+        'Основной цвет': ['Black', 'Gray', 'Cinnamon'],
+        'Популяция': [fur_color_black, fur_color_gray, fur_color_cinnamon]
+    }
+
+    new_dataframe = pandas.DataFrame(data_dict)
+    new_dataframe.to_csv('example_result.csv')
